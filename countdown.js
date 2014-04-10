69a9eb2057d39d3223928e101cd06452099dc15b
Dominik Schreiber <domischreib@gmail.com>
2014-04-10 09:04:57 +0200

    switched from own-implemented tick()-function to timer.js

---
+++ countdown.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+var cli = require('cli')
+  , timestring = require('timestring')
+  , Timer = require('timer.js')
+  , parsed
+  , end;
+
+cli.parse({});
+
+cli.main(function(args, options) {
+    var parsed = {}, end;
+
+    cli.setUsage('countdown.js <time> (i.e. `countdown.js 1h12m`)');
+    if (args.length !== 1) { cli.fatal(cli.getUsage()); }
+
+    parsed.time = args[0].parseTime();
+    end = new Date(Date.now() + parsed.time * 1000);
+
+    new Timer({
+            tick: .5,
+            onstart: function() { cli.info('Countdown ends at ' + end.toLocaleTimeString()); },
+            ontick: function(sec) { cli.progress((parsed.time - sec) / parsed.time); },
+            onend: function() { cli.info('Countdown has ended.\u0007'); }
+        }).start(parsed.time);
+});
